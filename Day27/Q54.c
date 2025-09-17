e66a617713aae35804cb410ab51e2b1f7ca58e85
PrakharGoyal_coder <prakhargoyal740@gmail.com>
2025-09-17 15:40:08 +0530

    Modifiedas Question was changed

--- Day27/Q54.c
+++ Day27/Q54.c
@@ -1,14 +1,12 @@
-//Q54: Write a program to print the following pattern:
-/*
-*
+/*Q54: Write a program to print the following pattern:
 
-***
-
-*****
+   *
+  ***
+ *****
 *******
-***
-
-*
+ *****
+  ***
+   *
 
 Sample Test Cases:
 Input 1:
@@ -19,43 +17,35 @@ Pattern with layers of stars as shown.
 */
 
 #include<stdio.h>
-int main(){
-for(int i=1;i<=9;i++)
-{
-    if(i==1||i==9)
-    {
-        for(int j=1;j<=1;j++)
-           printf("*");
-           printf("\n");
-    }
-    else if(i==3||i==7)
-    {
-          for(int j=1;j<=3;j++)
-             printf("*");
-        printf("\n");
-    }
-    else if(i==5)
-    {
-        for(int j=1;j<=5;j++)
-           printf("*");
+int main()
+{    int n=4;
+    int stars=0;
+    //upper triangle
+    for(int i=1;i<=n;i++)
+    {  
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=(2*i-1);k++)
+        {
+            printf("*");
+        }
        printf("\n");
     }
-    else if(i==6)
+    //lower triangle
+    for(int i=n-1;i>=1;i--)
     {
-        for(int j=1;j<=7;j++)
-          printf("*");
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=(2*i-1);k++)
+        {
+            printf("*");
+        }
         printf("\n");
     }
-     else
-     {
-         printf("\n");
-     }
 
-    // if(i!=1||i!=4||i!=5||i!=9)
-    // {
-    //     printf("\n");
-    // }
-
-}
-return 0;
+    return 0;
 }
